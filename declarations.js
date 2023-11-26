27c647d5776bf24dceaa855a6574c56baf954aff
layla-lili <laylaisa.211@gmail.com>
2023-11-26 11:05:17 +0300

    obj

---
+++ declarations.js
@@ -0,0 +1,17 @@
+const escapeStr="/`//\\\/\"\' "
+const arr=[4,2]
+const obj={
+str: "Cute Js ^v^",
+num: 123456,
+bool: true,
+undef: undefined
+}
+const nested={
+arr: [4, undefined , '2'],
+obj:{
+str: "",
+num:123,
+bool: true
+}
+} 
+Object.freeze(obj,nested)
