6c96ef7816edb7c31ed8c6de6de2e252d1d8e671
layla-lili <laylaisa.211@gmail.com>
2023-11-26 11:24:45 +0300

    obj

--- declarations.js
+++ declarations.js
@@ -7,11 +7,12 @@ bool: true,
 undef: undefined
 }
 const nested={
-arr: [4, undefined , 2],
+arr: [4, undefined , '2'],
 obj:{
 str: "",
 num:123,
 bool: true
 }
 } 
+console.log(nested.arr[1])
 Object.freeze(obj,nested)
