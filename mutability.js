2cccb98d08b0be3fe5ec1df00ec4540d2a6f5d9b
layla-lili <laylaisa.211@gmail.com>
2023-11-26 14:01:41 +0300

    mutable

--- mutability.js
+++ mutability.js
@@ -1,6 +1,6 @@
 const samePerson=person
 const newage=person.age+1
 const clone1={...person,age:newage,country:'FR'}
-const clone2={...person,age:newage,country:'FR'}
+const clone2=JSON.parse(JSON.stringify(person))
 
 
