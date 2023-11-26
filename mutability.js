b0af4ca4a2f28feea8f54021e205278dcaa21617
layla-lili <laylaisa.211@gmail.com>
2023-11-26 15:01:57 +0300

    mutable shalow and deep copy

--- mutability.js
+++ mutability.js
@@ -1,5 +1,5 @@
-const samePerson={...person}
-const clone1=person
-const clone2=clone1
-clone1.age=clone1.age+1
-clone1.country='FR'
+const samePerson=person
+const clone1={...person}
+const clone2={...person}
+person.age=person.age+1
+person.country='FR'
