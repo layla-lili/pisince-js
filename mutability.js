f471038b14a16eb342d6ddc5839c04941678a651
layla-lili <laylaisa.211@gmail.com>
2023-11-26 12:19:06 +0300

    mutable

--- mutability.js
+++ mutability.js
@@ -1,4 +1,13 @@
+const person = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+  }
+const samePerson=person
 const newage=person.age+1
   const clone1={...person,age:newage,country:'FR'}
   const clone2={...person,age:newage,country:'FR'}
-  const samePerson=person
+
+
+  console.log(person===samePerson);
+
