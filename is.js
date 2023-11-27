c63aca54fb0371cf5e8055aa719fce2ee6138425
Layla abdulla <laylablockchainjs@Laylas-MacBook-Air.local>
2023-11-28 00:22:26 +0300

    is

--- is.js
+++ is.js
@@ -1,14 +1,11 @@
-
-    num= (value) => typeof value === 'number'
-    nan= (value) => Number.isNaN(value)
-    str= (value) => typeof value === 'string'
-    bool= (value) => typeof value === 'boolean'
-    undef= (value) => typeof value === 'undefined'
-    def= (value) => typeof value !== 'undefined'
-    arr= (value) => Array.isArray(value)
-    obj= (value) => typeof value === 'object' && value !== null
-    fun= (value) => typeof value === 'function'
-    truthy= (value) => Boolean(value)
-    falsy=(value) => !value
-  
- 
+is.num = (n) => typeof n === "number";
+is.nan = (n) => Number.isNaN(n);
+is.str = (n) => typeof n === "string";
+is.bool = (n) => typeof n === "boolean";
+is.undef = (n) => n === undefined;
+is.def = (n) => !is.undef(n);
+is.arr = (n) => Array.isArray(n);
+is.obj = (n) => typeof n === "object" && !is.fun(n) && !is.arr(n) && n !== null;
+is.fun = (n) => typeof n === "function";
+is.truthy = (n) => !!n;
+is.falsy = (n) => !n;
