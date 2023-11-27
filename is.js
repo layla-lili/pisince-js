89b363c5e633722db9828a0003e0e321081ba6f7
Layla abdulla <laylablockchainjs@Laylas-MacBook-Air.local>
2023-11-27 23:55:25 +0300

    is

---
+++ is.js
@@ -0,0 +1,13 @@
+const is = {
+    num: value => typeof value === 'number',
+    nan: value => Number.isNaN(value),
+    str: value => typeof value === 'string',
+    bool: value => typeof value === 'boolean',
+    undef: value => typeof value === 'undefined',
+    def: value => typeof value !== 'undefined',
+    arr: value => Array.isArray(value),
+    obj: value => typeof value === 'object' && value !== null,
+    fun: value => typeof value === 'function',
+    truthy: value => Boolean(value),
+    falsy: value => !Boolean(value)
+  };
