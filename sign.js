208554feb11490b5e68d378fd5981b49b67310bd
Layla abdulla <laylablockchainjs@Laylas-MacBook-Air.local>
2023-11-27 23:44:01 +0300

    sign

--- sign.js
+++ sign.js
@@ -3,4 +3,6 @@ function sign(n){
     if (n<0) return -1
     if (n===0) return 0
 }
-const sameSign=(x,y)=>{sign(x)===sign(y)}
+const sameSign=(x,y)=>sign(x)===sign(y)
+
+console.log(sameSign(-1,-2))
