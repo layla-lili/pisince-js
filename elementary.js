602baadaecce52f2395de564fa622ac6b89df6f9
Layla abdulla <laylablockchainjs@Laylas-MacBook-Air.local>
2023-11-29 00:14:24 +0300

    elemntry

--- elementary.js
+++ elementary.js
@@ -1,23 +1,31 @@
-const multiply = (a,b)=>{
-    let result=0
-    let c=0
-    while(c<b){
-        c++
-        result=result+a
-    }
-    return result
-}
+const multiply = (a,b)=>b<0 || a<0 ? eval(a+String.fromCharCode(42)+b) :-eval(a+String.fromCharCode(42)+b)
+const divide = (a, b) => b === 0 ? 0 : (b < 0 || a < 0 ? eval(a + String.fromCharCode(47) + b) : -eval(a + String.fromCharCode(47) + b));
+const modulo = (a, b) => b === 0 ? 0 : (b < 0 || a < 0 ? eval(a + String.fromCharCode(37) + b) : -eval(a + String.fromCharCode(37) + b));
 
 
- const divide = (a,b)=>{
-    let c=0
-    let def=a
-    while(def>=b){
-        def-=b 
-        c++
-    }
-    return c
- }
+
+//{
+    // let result=0
+    // let c=0
+    // while(c<b){
+    //     c++
+    //     result=result+a
+    // }
+    // return result
+//}
+
+
+
+
+//  {
+//     let c=0
+//     let def=a
+//     while(def>=b){
+//         def-=b 
+//         c++
+//     }
+//     return c
+//  }
 //  function divideWithoutDivisionSign(dividend, divisor) {
 //     // Calculate the reciprocal of the divisor
 //     const reciprocal = 1 / divisor;
@@ -29,9 +37,10 @@ const multiply = (a,b)=>{
 //   }
 
 
- const modulo = (a,b)=>{
- return a- multiply(b,divide(a,b))
- }
+
+//  {
+//  return a - multiply(b,divide(a,b))
+//  }
 
 
 
