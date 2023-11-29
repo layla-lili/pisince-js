dff79615724497fa3a67af66868df53c135c8b27
Layla abdulla <laylablockchainjs@Laylas-MacBook-Air.local>
2023-11-29 20:09:01 +0300

    elemntry

--- elementary.js
+++ elementary.js
@@ -1,10 +1,8 @@
-const multiply = (a,b)=>b === 0 ? 0 : (b < 0 || a) && !(b < 0 && a<0) ? -Math.abs(eval(a+String.fromCharCode(42)+b)) : Math.abs(eval(a+String.fromCharCode(42)+b))
-const divide = (a, b) => b === 0 ? 0 : (b < 0 || a < 0) && !(b < 0 && a<0) ? -Math.abs(eval(a + String.fromCharCode(47) + b)) : Math.abs(eval(a + String.fromCharCode(47) + b))
-const modulo = (a, b) => b === 0 ? 0 : (b < 0 || a < 0) && !(b < 0 && a<0) ? -Math.abs(eval(a + String.fromCharCode(37) + b)) : Math.abs(eval(a + String.fromCharCode(37) + b))
+const multiply = (a,b) => b === 0 ? 0 : (b < 0 || a<0) && !(b < 0 && a<0) ? -Math.abs(eval(a+String.fromCharCode(42)+b)) : Math.abs(eval(a+String.fromCharCode(42)+b))
+const divide = (a, b) => b === 0 ? 0 : (b < 0 || a < 0) && !(b < 0 && a<0) ? -Math.abs(Math.ceil(eval(a + String.fromCharCode(47) + b))) : Math.abs(Math.floor(eval(a + String.fromCharCode(47) + b)))
+const modulo = (a, b) => b === 0 ? 0 : a - multiply(b,divide(a,b))
 
 
-
-console.log(multiply(-22, -123) )
 
 //{
     // let result=0
