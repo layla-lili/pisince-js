89b9ed9b26785222587b7a1676a8a7255095ab9f
Layla abdulla <laylablockchainjs@Laylas-MacBook-Air.local>
2023-11-30 00:48:28 +0300

    slice

--- slicer.js
+++ slicer.js
@@ -1,10 +1,9 @@
 const slice =(strArr,sIndex,eIndex)=>{
     const newarr=[]
-    const length = Math.min(eIndex, strArr.length);
+    const length = eIndex || strArr.length
     for (let i = sIndex; i < length; i++) {
         newarr.push(strArr[i]);
     }
     return newarr;
-
 }
-console.log("🚀 ~ file: slicer.js:2 ~ slice:", slice([1,2,3,4,5],2,4))
+
