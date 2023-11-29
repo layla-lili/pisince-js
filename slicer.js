fcc04ba8d914b82f3ad0eb5b2407c2cfd9a16771
Layla abdulla <laylablockchainjs@Laylas-MacBook-Air.local>
2023-11-30 01:28:29 +0300

    slice

--- slicer.js
+++ slicer.js
@@ -1,8 +1,11 @@
 const slice =(strArr,sIndex,eIndex)=>{
     var slicer
     typeof(strArr) === Array? slicer=[]: slicer=""
-    const length = eIndex || strArr.length
+    
+    var length = eIndex || strArr.length
     sIndex<0?sIndex=(length+sIndex):sIndex
+    if(eIndex<0) length=-1*(length+eIndex)
+    console.log("🚀 ~ file: slicer.js:8 ~ slice ~ length:", length)
     for (let i = sIndex; i < length; i++) {
         if (typeof(strArr) === Array){
         slicer.push(strArr[i]);
@@ -13,5 +16,5 @@ const slice =(strArr,sIndex,eIndex)=>{
     
     return slicer;
 }
-console.log("🚀 ~ file: slicer.js:9 ~ slice ~ slice:", slice('abcdef', -2))
+console.log("🚀 ~ file: slicer.js:9 ~ slice ~ slice:", slice('abcdef', 0, -2))
 
