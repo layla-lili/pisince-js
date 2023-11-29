db9fccc5a7ef88249b41c7372e9f09267d828492
Layla abdulla <laylablockchainjs@Laylas-MacBook-Air.local>
2023-11-30 00:12:56 +0300

    indexof

--- index-of.js
+++ index-of.js
@@ -1,10 +1,9 @@
 const indexOf = (arr, arg, start) => {
-    var s
-    start ? s = start : s = 0
+ 
 
-    for (let i = s; i < arr.length; i++) {
+    for (let i = start || 0 ; i < arr.length; i++) {
         const element = arr[i];
-        if (element === arg) {
+        if (String(element) === String(arg)) {
             return i
         }
     }
@@ -16,10 +15,19 @@ const lastIndexOf = (arr, arg, start) => {
     var index
     start ? s = start : s = 0
 
-    for (let i = s; i < arr.length; i++) {
+    for (let i = start || arr.length-1; i > 0; i--) {
         const element = arr[i];
-        index = arg === element ? i : -1
+        if (String(element) === String(arg)) {
+            return i
+        }
     }
-    return index
+    return -1
 }
+]
+
 const includes = (arr, arg) => indexOf(arr, arg) != -1 ? true : false
+// const bigArray = [...Array(999).keys(), ...Array(999).keys()
+// let array=[1, 'r', 3, 4, 't', 4, 't', 2, 1]
+// console.log("🚀 ~ file: index-of.js:25 ~ lastIndexOf ~ lastIndexOf:", lastIndexOf(bigArray, ctx.rn))
+
+
