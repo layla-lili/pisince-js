2205834fa20db297e6eeef04ac64d334c0971e45
Layla abdulla <laylablockchainjs@Laylas-MacBook-Air.local>
2023-11-30 00:13:49 +0300

    indexof

--- index-of.js
+++ index-of.js
@@ -23,10 +23,10 @@ const lastIndexOf = (arr, arg, start) => {
     }
     return -1
 }
-]
+
 
 const includes = (arr, arg) => indexOf(arr, arg) != -1 ? true : false
-// const bigArray = [...Array(999).keys(), ...Array(999).keys()
+// const bigArray = [...Array(999).keys(), ...Array(999).keys()]
 // let array=[1, 'r', 3, 4, 't', 4, 't', 2, 1]
 // console.log("🚀 ~ file: index-of.js:25 ~ lastIndexOf ~ lastIndexOf:", lastIndexOf(bigArray, ctx.rn))
 
