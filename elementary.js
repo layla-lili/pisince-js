699608d8ef08d073835b56e4c099f0c90459f6ce
Layla abdulla <laylablockchainjs@Laylas-MacBook-Air.local>
2023-11-29 10:47:22 +0300

    elemntry

--- elementary.js
+++ elementary.js
@@ -1,9 +1,10 @@
-const multiply = (a,b)=>b<0 || a<0 ? eval(a+String.fromCharCode(42)+b) :-eval(a+String.fromCharCode(42)+b)
-const divide = (a, b) => b === 0 ? 0 : (b < 0 || a < 0 ? eval(a + String.fromCharCode(47) + b) : -eval(a + String.fromCharCode(47) + b));
-const modulo = (a, b) => b === 0 ? 0 : (b < 0 || a < 0 ? eval(a + String.fromCharCode(37) + b) : -eval(a + String.fromCharCode(37) + b));
+const multiply = (a,b)=>b === 0 ? 0 : (b < 0 || a) && !(b < 0 && a<0) ? -Math.abs(eval(a+String.fromCharCode(42)+b)) : Math.abs(eval(a+String.fromCharCode(42)+b))
+const divide = (a, b) => b === 0 ? 0 : (b < 0 || a < 0) && !(b < 0 && a<0) ? -Math.abs(eval(a + String.fromCharCode(47) + b)) : Math.abs(eval(a + String.fromCharCode(47) + b))
+const modulo = (a, b) => b === 0 ? 0 : (b < 0 || a < 0) && !(b < 0 && a<0) ? -Math.abs(eval(a + String.fromCharCode(37) + b)) : Math.abs(eval(a + String.fromCharCode(37) + b))
 
 
 
+console.log(multiply(-22, -123) )
 
 //{
     // let result=0
