a8e5134b33784dc10c4f4ad496f0a7506890e2a8
Layla abdulla <laylablockchainjs@Laylas-Air.reboot.internal>
2023-11-30 16:27:14 +0300

    letterspacenumber

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,2 +1,2 @@
-const letterSpaceNumber=(str)=>str.test(/\w\s\d/)
+const letterSpaceNumber=(str)=>str.match(/\w\s\d/)[0]
 console.log("🚀 letterSpaceNumber:", letterSpaceNumber("example 1, example 2"))
