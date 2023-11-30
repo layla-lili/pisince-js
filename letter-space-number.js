85d5de78782274a7d502ff1380791ab28f3d8546
Layla abdulla <laylablockchainjs@Laylas-Air.reboot.internal>
2023-11-30 16:37:45 +0300

    letterspacenumber

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,2 +1,3 @@
-const letterSpaceNumber=(str)=>str.match(/\w \d(?!\w)/gi)
+const letterSpaceNumber=(str)=>str.match(/\w \d(?!\w)/gi) || []
 
+console.log("🚀 letterSpaceNumber:", letterSpaceNumber("I like 7up."))
