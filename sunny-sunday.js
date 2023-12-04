cb7bf5e1deba3935f333aa45f271064c25ff55c1
Layla abdulla <laylablockchainjs@Laylas-Air.reboot.internal>
2023-12-04 20:06:00 +0300

    sunny sunday

---
+++ sunny-sunday.js
@@ -0,0 +1,7 @@
+function sunnySunday(date) {
+    const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const epoch = new Date(1, 0, 1);  // Epoch is 01/01/0001
+    const daysDiff = Math.floor((date - epoch) / (1000 * 60 * 60 * 24));
+    const weekdayIndex = (daysDiff % 7) % 6;  // Modulo 6 to account for the eliminated Sunday
+    return weekdays[weekdayIndex];
+  }
