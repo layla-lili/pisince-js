7980ce94f2b8a4189f3299ef7ed7537ee3b5a28e
labdulla <laylaisa.211@gmail.com>
2023-12-06 21:13:38 +0300

    every some

--- long-words.js
+++ long-words.js
@@ -2,4 +2,5 @@ const longWords=(strarr)=>strarr.every((ar)=>typeof ar === "string" && ar.length
 
 const oneLongWord=(strarr)=>strarr.some((ar)=>typeof ar === "string" && ar.length >=10)
 
-const noLongWords=(strarr)=>!(strarr.every((ar)=>typeof ar === "string" && ar.length >=7))
+const noLongWords=(strarr)=>!(strarr.some((ar)=>typeof ar === "string" && ar.length >=7))
+
