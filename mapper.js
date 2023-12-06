d4fdf37cd62f51fa304339122b3ca8d4ed6aab6a
Layla abdulla <laylablockchainjs@Laylas-MacBook-Air.local>
2023-12-06 23:37:10 +0300

    compltet

--- mapper.js
+++ mapper.js
@@ -1,2 +1,2 @@
-const map = (arr,fu, val)=>{for(let i=0; i<arr.length;i++){fu(arr[i],i,arr)}}
-const flatMap = (arr, fu, val)=>{for(let i=0; i<arr.length; i++){fu(...arr[i],i,arr)}}
+const map = (arr,fu)=>{let r=[]; for(let i=0; i<arr.length;i++){r.push(fu(arr[i],i,arr))} return r}
+const flatMap = (arr, fu)=> arr.reduce((v1,v2)=>{let r = fu(v2); return v1.concat(r)},[])
