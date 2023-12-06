69a1491da5a2db3900122cd33632faae621431a4
labdulla <laylaisa.211@gmail.com>
2023-12-06 20:53:34 +0300

    every some

--- long-words.js
+++ long-words.js
@@ -1,14 +1,5 @@
-
-let arr1 = ['fill', 'carbon', 'chart', 'glare', 'express']
-let arr2 = ['double', 'afford', 'coalition', 'reaction', 'persist']
-let arr3 = ['leak', 'talk', 'bite', 'slip', 'free']
-let arr4 = ['fixture', 'opponent', 'coincide', 'residential', 'relaxation']
-
 const longWords=(strarr)=>strarr.every((ar)=>typeof ar === "string" && ar.length >=5)
-console.log("🚀 ~ file: long-words.js:8 ~ longWords:", longWords(arr2))
 
 const oneLongWord=(strarr)=>strarr.some((ar)=>typeof ar === "string" && ar.length >=10)
-console.log("🚀 ~ file: long-words.js:11 ~ oneLongWord:", oneLongWord(arr4))
 
 const noLongWords=(strarr)=>!(strarr.every((ar)=>typeof ar === "string" && ar.length >=5))
-console.log("🚀 ~ file: long-words.js:14 ~ noLongWords:", noLongWords(arr3))
