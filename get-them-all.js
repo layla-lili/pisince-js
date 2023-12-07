a3e86b77d340a8c76c617197f46e9845daee7114
Layla abdulla <laylablockchainjs@Laylas-Air.reboot.internal>
2023-12-07 18:17:59 +0300

    compltet

---
+++ get-them-all.js
@@ -0,0 +1,6 @@
+const getArchitects = [document.querySelectorAll("body a"), document.querySelectorAll("body span") ]
+const getClassical = [document.querySelectorAll("a.classical"),document.querySelectorAll("a:not(.classical")]
+const getActive = [document.querySelectorAll("a.classical.active"), document.querySelectorAll("a.classical:not(active")]
+const getBonannoPisano = [document.getElementById("BonannoPisano"), document.querySelectorAll("a.classical.active")]
+
+export {getArchitects,getClassical, getActive, getBonannoPisano}
