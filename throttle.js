43babad6a0c5612b67e3158563aae759e759e2bf
Layla <laylaisa.211@gmail.com>
2023-12-13 01:18:03 +0300

    fusion

--- throttle.js
+++ throttle.js
@@ -15,31 +15,33 @@ const throttle = (func, delay)=>{
     };
 }
 //the leading is the default classic above implmentation traling when its true and leading is false will be excuted
-const opThrottle = (func, delay, option={trailing:true, leading:true})=>{
+const opThrottle = (func, delay, options = { trailing: true, leading: true }) => {
     let timeoutId;
-    let isExecuting;
-
+    let isExecuting = false;
+  
     return function (...args) {
-        const {leading,trailing}=option;
-        //helper function for the trailing recursivvly start the timer
-        const waitfn = () =>{
-            if(trailing && isExecuting){
-                func.apply(this, args)
-                isExecuting = null
-                timeoutId = setTimeout(waitfn, delay)
-            }else{
-                timeoutId =null
-            }
-        }
-       
-      if (!timeoutId && leading){
-        func.apply(this, args)
-      }else{
-        isExecuting=args
-      }
-      if(!timeoutId){
-        timeoutId=setTimeout(waitfn, delay)
-      }
-        
+      const { leading, trailing } = options;
+  
+      const executeFunction = () => {
+        func.apply(this, args);
+        isExecuting = false;
+        timeoutId = null;
       };
-}
+  
+      if (!timeoutId) {
+        if (leading) {
+          executeFunction();
+        }
+        if (trailing) {
+          isExecuting = true;
+          timeoutId = setTimeout(() => {
+            if (trailing && isExecuting) {
+              executeFunction();
+            }
+          }, delay);
+        }
+      } else {
+        isExecuting = true;
+      }
+    };
+  };
