2c2719000681f2fc04d2815c7c3b5452315db888
Layla <laylaisa.211@gmail.com>
2023-12-13 01:30:55 +0300

    fusion

--- throttle.js
+++ throttle.js
@@ -15,36 +15,34 @@ const throttle = (func, delay)=>{
     };
 }
 //the leading is the default classic above implmentation traling when its true and leading is false will be excuted
-const opThrottle = (func, delay, options = { trailing: true, leading: true }) => {
-    let timeoutId;
-    let isExecuting = false;
-    let isExecutedInLeading = false;
-  
-    return function (...args) {
-      const { leading, trailing } = options;
-  
-      const executeFunction = () => {
-        func.apply(this, args);
-        isExecuting = false;
-        timeoutId = null;
-        isExecutedInLeading = false;
-      };
-  
-      if (!timeoutId) {
-        if (leading) {
-          executeFunction();
-          isExecutedInLeading = true;
+const opThrottle = (fn, delay, { leading = false, trailing = true } = {}) => {
+    let last = 0;
+    let timer = null;
+    
+    return function () {
+        const now = +new Date();
+        
+        // If leading is false and this is the first invocation, update the last timestamp
+        if (!last && leading === false) {
+            last = now;
         }
-        if (trailing) {
-          isExecuting = true;
-          timeoutId = setTimeout(() => {
-            if (trailing && isExecuting && !isExecutedInLeading) {
-              executeFunction();
+        
+        // If the time since the last invocation exceeds the delay, execute the function
+        if (now - last > delay) {
+            if (timer) {
+                clearTimeout(timer);
+                timer = null;
             }
-          }, delay);
+            fn.apply(this, arguments);
+            last = now;
+        } 
+        // If trailing is not false and there is no timer, set a timer to execute the function after the delay
+        else if (!timer && trailing !== false) {
+            timer = setTimeout(() => {
+                fn.apply(this, arguments);
+                last = +new Date();
+                timer = null;
+            }, delay);
         }
-      } else {
-        isExecuting = true;
-      }
     };
-  };
+};
