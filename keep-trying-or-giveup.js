4aba2789ec75c2208193e8fce1e510bf20794814
Layla <laylaisa.211@gmail.com>
2023-12-14 22:13:56 +0300

    fusion

---
+++ keep-trying-or-giveup.js
@@ -0,0 +1,26 @@
+const retry = (count, callback)=> {
+    return async function (...args) {
+      let attempt = 0;
+      while (attempt <= count) {
+        try {
+          return await callback(...args);
+        } catch (error) {
+          attempt++;
+          if (attempt > count) {
+            throw new Error('Exceeded maximum number of retries');
+          }
+        }
+      }
+    };
+  }
+  
+  const timeout = (delay, callback)=> {
+    return function (...args) {
+      return Promise.race([
+        callback(...args),
+        new Promise((_, reject) =>
+          setTimeout(() => reject(new Error('Timeout')), delay)
+        ),
+      ]);
+    };
+  }
