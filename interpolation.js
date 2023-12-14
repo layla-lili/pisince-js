11e6cc1dfdbc1fe78e6ee5e5109e9c267ebf8c60
Layla <laylaisa.211@gmail.com>
2023-12-14 19:26:12 +0300

    fusion

--- interpolation.js
+++ interpolation.js
@@ -1,20 +1,22 @@
-function interpolation({
+const interpolation = ({
     step = 0,
     start = 0,
     end = 0,
     callback = () => {},
     duration = 0,
-} = {}) {
+  } = {}) => {
     const delta = (end - start) / step;
     let current = start;
     let i = 0;
+  
     const timer = setInterval(() => {
-        if (i < step) {
-            callback([current, (duration / step) * (i + 1)]);
-            current += delta;
-            i++;
-        } else {
-            clearInterval(timer);
-        }
+      if (i < step) {
+        const time = (duration / step) * (i + 1);
+        callback([current, time]);
+        current += delta;
+        i++;
+      } else {
+        clearInterval(timer);
+      }
     }, duration / step);
-}
+  };
