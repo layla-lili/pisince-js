bdcbff4efe08ded43f278cac30a25e4d7f17c7ec
Layla <laylaisa.211@gmail.com>
2023-12-18 00:55:07 +0300

    node

---
+++ verydisco.mjs
@@ -0,0 +1,31 @@
+// const arg =process.argv[2]
+
+// const first = arg.split('').slice(0,Math.ceil(arg.length/2))
+// const second = arg.split('').slice(arg.length-(Math.ceil(
+//     arg.length/2)))
+// const reJoin = second.concat(first)
+// const reJoinString =reJoin.join(' ')
+//     console.log(reJoinString)
+const input = process.argv[2];
+
+function makeDiscoWord(word) {
+  const length = Math.ceil(word.length / 2);
+  const firstPart = word.slice(0, length);
+  const secondPart = word.slice(length);
+  return secondPart + firstPart;
+}
+
+function makeDiscoSentence(sentence) {
+  const words = sentence.split(' ');
+  const discoWords = words.map(() => 'verydisco');
+  return discoWords.join(' ');
+}
+
+let result;
+if (input.includes(' ')) {
+  result = makeDiscoSentence(input);
+} else {
+  result = makeDiscoWord(input);
+}
+
+console.log(result);
