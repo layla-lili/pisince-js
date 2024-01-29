ea074dfdb1a4ecd42c987751ef13337f55f0c8b5
Layla <laylaisa.211@gmail.com>
2024-01-30 00:35:34 +0300

    primitive

---
+++ declarations.js
@@ -0,0 +1,22 @@
+const escapeStr="/`//\\\/\"/'"
+const arr=[4,'2']
+const obj={
+str: "Cute Js ^v^",
+num: 123456,
+bool: true,
+undef: undefined
+}
+
+Object.freeze(obj)
+Object.freeze(arr)
+
+const nested={
+arr: Object.freeze([4, undefined , '2']),
+obj:Object.freeze({
+str: "",
+num:123,
+bool: true
+})
+} 
+Object.freeze(nested)
+Object.freeze(nested.obj)
