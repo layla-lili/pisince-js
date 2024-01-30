23851670b9846a950154d0874b4085cb4db023e7
Layla <laylaisa.211@gmail.com>
2024-01-30 23:49:54 +0300

    methods

---
+++ method-man.js
@@ -0,0 +1,7 @@
+const words = (str)=>str.split(' ')
+const sentence = (str)=>str.join(' ')
+const yell = (str)=>str.toUpperCase()
+const whisper = (str)=>str.toLowerCase()
+const capitalize = (str)=>str.charAt(0).toUpperCase()+str.slice(1)
+
+
