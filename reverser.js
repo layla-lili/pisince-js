832701b6f7fb14ab4258fe888ca225d90196227e
Layla <laylaisa.211@gmail.com>
2024-02-01 01:26:08 +0300

    revers

--- reverser.js
+++ reverser.js
@@ -1,8 +1,9 @@
 const reverse = (arr) => {
     let arrr = []
     let temp = []
+    if (typeof arr === "string") {
     temp = arr.split(" ");
-
+    }
     while (temp.length) {
         arrr.push(temp.pop())
     }
