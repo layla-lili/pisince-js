46d2fa42b9adbcaa55e7bbad7548673b235bca92
Layla <laylaisa.211@gmail.com>
2024-01-31 23:50:18 +0300

    slicer

---
+++ slicer.js
@@ -0,0 +1,25 @@
+const slice = (strArr, sIndex, eIndex) => {
+    var slicer
+    if (eIndex === undefined) {
+        eIndex = strArr.length;
+    }
+
+    if (typeof strArr === "object") {
+        slicer = []; // Initialize slicer as an empty array
+    } else {
+        slicer = ""; // Initialize slicer as an empty string
+    }
+     
+    if (sIndex < 0) sIndex = strArr.length + sIndex;
+
+    if (eIndex < 0) eIndex = strArr.length + eIndex;
+    
+    for (let i = sIndex; i < eIndex; i++) {
+        if (typeof strArr === "object") {
+            slicer.push(strArr[i]);
+        } else {
+            slicer += strArr[i]
+        }
+    }
+    return slicer;
+}
