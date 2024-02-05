c93f3169308b896f73646d0a613cb133f6f850db
Layla <laylaisa.211@gmail.com>
2024-02-05 19:52:07 +0300

    date

--- date-is.js
+++ date-is.js
@@ -1,7 +1,5 @@
-const isValid = (date)=>  !(date instanceof Date) || isNaN(date.getTime())?false:true
-
+const isValid = (date)=> (new Date(date).toString()) === "Invalid Date" && (!(date instanceof Date) || isNaN(date.getTime()) )?false:true
 const isAfter = (date1, date2)=> date1 > date2? true : false 
-
 const isBefore =  (date1, date2)=> date1 < date2? true : false 
 const isFuture = (date) => isValid(date) && Date.now() - date.getTime() < 0 ? true : false
 const isPast = (date) => isValid(date) && Date.now() - date.getTime() >  0 ? true : false
