5c63b4d360c35d209c0f963c135b12f3a2b8d50a
Layla <laylaisa.211@gmail.com>
2024-02-05 22:51:25 +0300

    datetime

--- rebecca-black.js
+++ rebecca-black.js
@@ -1,11 +1,14 @@
 const isFriday = (date) => new Date(date).getDay() === 5;
 
-const isWeekend = (date) => new Date(date).getDay() === 0 || new Date(date).getDay() === 5
+const isWeekend = (date) => {
+    var day = new Date(date).getDay();
+   return day === 0 || day === 5
+}
 
 
 const isLeapYear = (date) => (new Date(date).getFullYear()%4 === 0 && new Date(date).getFullYear()%100 !== 0) || new Date(date).getFullYear()% 400 === 0;
 
 const isLastDayOfMonth = (date) =>  {
     var d = new Date(date); 
-    new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate() === d.getDate()
+   return  new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate() === d.getDate()
 }
