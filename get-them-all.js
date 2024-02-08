576d400689f249bdf0e43897fede7edc82064817
Layla <laylaisa.211@gmail.com>
2024-02-08 20:24:04 +0300

    dom

--- get-them-all.js
+++ get-them-all.js
@@ -1,26 +1,6 @@
-const getArchitects = () => {
-    const architects = document.querySelectorAll('a.architect')
-    const nonArchitects = document.querySelectorAll(':not(a.architect)')
-    return [Array.from(architects), Array.from(nonArchitects)]
-  }
-  
-  const getClassical = () => {
-    const classicalArchitects = document.querySelectorAll('a.architect.classical')
-    const nonClassicalArchitects = document.querySelectorAll('a.architect:not(.classical)')
-    return [Array.from(classicalArchitects), Array.from(nonClassicalArchitects)]
-  }
-  
-  const getActive = () => {
-    const activeClassicalArchitects = document.querySelectorAll('a.architect.classical.active')
-    const nonActiveClassicalArchitects = document.querySelectorAll('a.architect.classical:not(.active)')
-    return [Array.from(activeClassicalArchitects), Array.from(nonActiveClassicalArchitects)]
-  }
+const getArchitects = ()=>[document.querySelectorAll("body a"), document.querySelectorAll("body span") ]
+const getClassical =  ()=>[document.querySelectorAll("a.classical"),document.querySelectorAll("a:not(.classical")]
+const getActive =  ()=>[document.querySelectorAll("a.classical.active"), document.querySelectorAll("a.classical:not(active")]
+const getBonannoPisano =  ()=> [document.getElementById("BonannoPisano"), document.querySelectorAll("a.classical.active")]
 
-  
-  const getBonannoPisano = () => {
-    const bonannoPisano = document.getElementById('BonannoPisano')
-    const activeClassicalArchitects = document.querySelectorAll('a.architect.classical.active:not(#BonannoPisano)')
-    return [bonannoPisano, Array.from(activeClassicalArchitects)]
-  }
-  
-  
+export {getArchitects,getClassical, getActive, getBonannoPisano}
