b8231eec541dc6a1f1e5889167d3a16018ecd107
Layla <laylaisa.211@gmail.com>
2024-02-13 20:48:10 +0300

    obj

---
+++ invert.js
@@ -0,0 +1,13 @@
+const invert = (obj)=>{
+    objj={}
+    let keys = Object.keys(obj)
+    keys.forEach(key=>{
+    let val=obj[key]
+    objj[val]=key
+       
+    })
+return objj
+}
+
+
+console.log("🚀 ~ invert:", invert({ firstName: 'John', lastName: 'Doe', age: 32 }))
