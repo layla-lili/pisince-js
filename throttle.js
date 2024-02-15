0023f5879e5952d50f7d4f2f9764e8c0efa87a97
Layla <laylaisa.211@gmail.com>
2024-02-16 01:39:08 +0300

    throttle

--- throttle.js
+++ throttle.js
@@ -1,33 +1,22 @@
-const throttle = (func, delay)=>{
-    let timeoutId
-    let isThrottled
-    
-    (...args)=> {
-     !isThrottled?(func.apply(this, args), isThrottled = Date.now()) :
-      clearTimeout(timeoutId)
-     timeoutId = setTimeout(()=>{
-      if(Date.now() - isThrottled >= delay){
-        func.apply(this, args)
-        isThrottled = Date.now()
-      }
-    }), delay - (Date.now()-isThrottled)
-    
+function throttle(fn, delay) {
+    let last = 0;
+    return function () {
+        const now = +new Date();
+        if (now - last > delay) {
+            fn.apply(this, arguments);
+            last = now;
+        }
     };
 }
-//the leading is the default classic above implmentation traling when its true and leading is false will be excuted
-const opThrottle = (fn, delay, { leading = false, trailing = true } = {}) => {
+
+function opThrottle(fn, delay, { leading = false, trailing = true } = {}) {
     let last = 0;
     let timer = null;
-    
-     ()=> {
+    return function () {
         const now = +new Date();
-        
-        // If leading is false and this is the first invocation, update the last timestamp
         if (!last && leading === false) {
             last = now;
         }
-        
-        // If the time since the last invocation exceeds the delay, execute the function
         if (now - last > delay) {
             if (timer) {
                 clearTimeout(timer);
@@ -35,9 +24,7 @@ const opThrottle = (fn, delay, { leading = false, trailing = true } = {}) => {
             }
             fn.apply(this, arguments);
             last = now;
-        } 
-        // If trailing is not false and there is no timer, set a timer to execute the function after the delay
-        else if (!timer && trailing !== false) {
+        } else if (!timer && trailing !== false) {
             timer = setTimeout(() => {
                 fn.apply(this, arguments);
                 last = +new Date();
@@ -45,4 +32,4 @@ const opThrottle = (fn, delay, { leading = false, trailing = true } = {}) => {
             }, delay);
         }
     };
-};
+}
